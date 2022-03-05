3875997808e7b438b9c4a65078e53cdfb0efd2c7
yzb <1435231027@qq.com>
2022-03-05 20:32:34 +0800

    feat: element-plus安装

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,13 @@ import web from './index.vue'
 import router from './router/index'
 // vuex
 import store from './store/index'
+// element-plus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 //
 const app = createApp(web)
 app.use(router)
-
 app.use(store)
+app.use(ElementPlus)
 // mount
 app.mount('#web')
