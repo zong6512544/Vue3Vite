a4806190766c3fdbdf39c8bb2b2a824c956e9931
yzb <1435231027@qq.com>
2022-03-30 12:39:29 +0800

    feat: 代码prettier格式化

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # Vue 3 + Typescript + Vite
 
 # GIT
+
 - feat：新功能（feature）
-- fix：修补bug
+- fix：修补 bug
 - docs：文档（documentation）
 - style： 格式（不影响代码运行的变动）
-- refactor：重构（即不是新增功能，也不是修改bug的代码变动）
+- refactor：重构（即不是新增功能，也不是修改 bug 的代码变动）
 - test：增加测试
 - chore：构建过程或辅助工具的变动
--- config/command/open.js
+++ config/command/open.js
@@ -8,7 +8,7 @@
 const { exec } = require('child_process')
 // 获取控制台参数(代理ip)
 const target = process.argv[2]
-console.log('\x1B[36m%s\x1B[0m','Project start, about to agent：',target)
+console.log('\x1B[36m%s\x1B[0m', 'Project start, about to agent：', target)
 // 执行启动项目命令，并带入参数
 const cmd = `npm run serve -- --env.target=${target}`
 const temp = exec(
