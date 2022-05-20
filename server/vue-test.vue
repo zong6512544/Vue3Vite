4965708d30bf951244e7184f68f6e12bef1958ae
yzb <1435231027@qq.com>
2022-05-20 14:29:39 +0800

    test: 测试husky、lint-staged

---
+++ server/vue-test.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="vue-test"></div>
+</template>
+<script lang="ts">
+import { onMounted } from 'vue'
+
+export default {
+  name: 'vue-test',
+
+  components: {},
+
+  setup(): any {
+    onMounted(() => {
+      console.log('test')
+    })
+  },
+
+  computed: {},
+
+  watch: {},
+
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+.vue-test {
+}
+</style>
